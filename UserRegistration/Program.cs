08471d8896106c53e69625dcd2a2780320011478
Divya-Bodake <divyabodake25@gmail.com>
2022-11-29 23:42:28 +0530

    [Divya]Add:Enter Valid FirstName

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select:-\n1.First Name \n 2.Break" );
+                int choice = Convert.ToInt32(Console.ReadLine());
+               
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your First name");
+                        string FirstName = Console.ReadLine();
+                        pattern.ValidFirstName(FirstName);
+                        break;
+                   case 2:
+                        flag = false;
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
