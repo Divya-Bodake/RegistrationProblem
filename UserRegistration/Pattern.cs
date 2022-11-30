5415384c044d42f7eb0b0bd6bc05a613ac32b7de
Divya-Bodake <divyabodake25@gmail.com>
2022-11-30 12:02:05 +0530

    [Divya]Add:Enter a Valid Last Name

--- UserRegistration/Pattern.cs
+++ UserRegistration/Pattern.cs
@@ -9,13 +9,22 @@ namespace UserRegistration
 {
     public class Pattern
     {
-        const string FirstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        const string FirstName = "^[A-Z][a-zA-Z]{2,}$";
+        const string LastName = "^[A-Z][a-zA-Z]{2,}$";
+
         public void ValidFirstName(string input)
         {
             if (Regex.IsMatch(input, FirstName))
                 Console.WriteLine("Your First name is  " +input);
             else
                 Console.WriteLine("Unvalid FirstName");
+        }
+        public void ValidLastName(string input)
+        {
+            if (Regex.IsMatch(input, LastName))
+                Console.WriteLine("Your Last name is  " +input);
+            else
+                Console.WriteLine("Unvalid LastName");
         }
     }
 }
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,7 +8,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Select:-\n1.First Name \n 2.Break" );
+                Console.WriteLine("Select:-\n1.First Name \n 2.LastName \n 3.Break  " );
                 int choice = Convert.ToInt32(Console.ReadLine());
                
 
@@ -19,7 +19,14 @@
                         string FirstName = Console.ReadLine();
                         pattern.ValidFirstName(FirstName);
                         break;
-                   case 2:
+
+                    case 2:
+                        Console.WriteLine("Enter your Last name");
+                        string LastName = Console.ReadLine();
+                        pattern.ValidLastName(LastName);
+                        break;
+
+                    case 3:
                         flag = false;
                         break;
 
